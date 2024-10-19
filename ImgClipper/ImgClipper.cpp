eee7f3d3d324d6a32b7e275ca991175661ab7220
motcom <motti234@qc5.so-net.ne.jp>
2024-10-19 13:48:34 +0900

    プロジェクト ファイルを追加します。

---
+++ ImgClipper/ImgClipper.cpp
@@ -0,0 +1,345 @@
+#include "ImgClipper.h"
+
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    helpTextEdit.setMarkdown(
+		"## How to operate\n"
+		"#### Normal mode\n"
+		"|Key|Description|\n"
+		"|---|---|\n"
+		"|f1|Open this help|\n"
+		"|p or ctrl+v|Push clipboard image to the list and display it|\n"
+		"|right or l|Move to the next image in the list|\n"
+		"|left or h|Move to the previous image in the list|\n"
+		"|z or +|Zoom in and switch to zoom mode|\n"
+		"|x or -|Zoom out and switch to zoom mode|\n"
+		"|t|Always on top|\n"
+		"|s or w|Save all list images to the picture folder|\n"
+		"|>|Decrease transparency|\n"
+		"|<|Increase transparency|\n"
+		"|tab|Toggle frameless window|\n"
+		"|esc|Exit|\n"
+		"\n"
+		"#### Zoom mode\n"
+		"|Key|Description|\n"
+		"|---|---|\n"
+		"|f1|Open this help|\n"
+		"|esc|Exit|\n"
+		"|z or +|Zoom in|\n"
+		"|x or -|Zoom out|\n"
+		"|right or l|Move image to the right|\n"
+		"|left or h|Move image to the left|\n"
+		"|up or k|Move image up|\n"
+		"|down or j|Move image down|\n"
+		"|tab|Toggle frameless window|\n"
+        );
+    helpTextEdit.setReadOnly(true);
+}
+MainWindow::~MainWindow() {}
+
+
+// Event 処理 start ------------------------------
+void MainWindow::keyPressEvent(QKeyEvent *key) {
+    switch (viewrMode) {
+    case ViewerMode::Normal:
+        normalBranch(key);
+        break;
+    case ViewerMode::Zoom:
+        zoomBranch(key);
+        break;
+    }
+}
+
+void MainWindow::normalBranch(QKeyEvent *key)
+{
+    switch(key->key())
+    {
+    case Qt::Key_F1:
+        helpTextEdit.show();
+        break;
+
+    // Zoom
+    case Qt::Key_Plus:
+    case Qt::Key_Z:
+        zoomFactor += ZOOM_DELTA;
+        if (zoomFactor >3.0) zoomFactor=3.0;
+        viewrMode = ViewerMode::Zoom;
+        update();
+        break;
+
+    case Qt::Key_X:
+    case Qt::Key_Minus:
+        zoomFactor -= ZOOM_DELTA;
+        if (zoomFactor < ZOOM_DELTA) zoomFactor = ZOOM_DELTA;
+        viewrMode = ViewerMode::Zoom;
+        update();
+        break;
+
+    // paste
+    case Qt::Key_V:
+        if(!(Qt::ControlModifier==key->modifiers()))
+            break;
+        Q_FALLTHROUGH();
+
+    case Qt::Key_P:
+        pasteImage();
+        break;
+
+    // window always top
+    case Qt::Key_T:
+        alwaysOnTop();
+        break;
+
+    // next
+    case Qt::Key_Right:
+    case Qt::Key_L:
+        next();
+        break;
+
+     // back
+    case Qt::Key_Left:
+    case Qt::Key_H:
+        back();
+        break;
+    case Qt::Key_Escape:
+    escape();
+        break;
+
+      //save
+    case Qt::Key_S:
+    case Qt::Key_W:
+    save();
+       break;
+
+    // setOpacity
+    case Qt::Key_Greater:
+        opacity += OPACITY_DELTA;
+        if(opacity > OPACITY_INIT) opacity = OPACITY_INIT;
+        setWindowOpacity(opacity);
+        break;
+
+    case Qt::Key_Less:
+        opacity -= OPACITY_DELTA;
+        if(opacity < 0) opacity = 0;
+        setWindowOpacity(opacity);
+        break;
+
+    // tab
+    case Qt::Key_Tab:
+        hideTabToggle();
+        break;
+   }
+}
+
+void MainWindow::zoomBranch(QKeyEvent *key)
+{
+
+    // state nomal
+    switch (key->key()) {
+
+    case Qt::Key_F1:
+        helpTextEdit.show();
+        break;
+
+    // Escape
+    case Qt::Key_Escape:
+       escape();
+        break;
+
+    // to nomal
+    case Qt::Key_N:
+    case Qt::Key_0:
+        zoomFactor = ZOOM_INIT;
+        imgOffset.setX(0);
+        imgOffset.setY(0);
+        viewrMode = ViewerMode::Normal;
+        update();
+        break;
+
+    // Zoom
+    case Qt::Key_Plus:
+    case Qt::Key_Z:
+        zoomFactor += ZOOM_DELTA;
+        if (zoomFactor > ZOOM_MAX) zoomFactor=ZOOM_MAX;
+        break;
+
+    case Qt::Key_Minus:
+    case Qt::Key_X:
+        zoomFactor -= ZOOM_DELTA;
+        if (zoomFactor < ZOOM_MIN) zoomFactor = ZOOM_MIN;
+        break;
+
+    // Right
+    case Qt::Key_Right:
+    case Qt::Key_L:
+        imgOffset.setX(imgOffset.x()-OFFSET_DELTA);
+        break;
+
+     // Left
+    case Qt::Key_Left:
+    case Qt::Key_H:
+        imgOffset.setX(imgOffset.x()+OFFSET_DELTA);
+        break;
+
+     // Up
+    case Qt::Key_J:
+        imgOffset.setY(imgOffset.y()-OFFSET_DELTA);
+        break;
+     // Down
+    case Qt::Key_K:
+        imgOffset.setY(imgOffset.y()+OFFSET_DELTA);
+        break;
+
+    // tab
+    case Qt::Key_Tab:
+        hideTabToggle();
+        break;
+
+    // setOpacity
+    case Qt::Key_Greater:
+        opacity += OPACITY_DELTA;
+        if(opacity > OPACITY_INIT) opacity = OPACITY_INIT;
+        setWindowOpacity(opacity);
+
+        break;
+
+    case Qt::Key_Less:
+        opacity -= OPACITY_DELTA;
+        if(opacity < 0) opacity = 0;
+        setWindowOpacity(opacity);
+        break;
+    }
+  update();
+}
+
+
+// render
+void MainWindow::paintEvent(QPaintEvent *) {
+    // 画像がなければ戻る
+    if (curIndex==-1) return;
+
+    // painter
+    QPainter painter(this);
+    QSize cntSize = contentsRect().size();
+    QImage scaledImg = imgList[curIndex].data()->scaled(cntSize,Qt::KeepAspectRatioByExpanding,Qt::SmoothTransformation);
+
+    int centerX = scaledImg.width() / 2 ;
+    int centerY = scaledImg.height() / 2;
+
+    QTransform transform;
+    transform.translate(centerX,centerY);
+    transform.scale(zoomFactor,zoomFactor);
+    transform.translate(-centerX,-centerY);
+    transform.translate(imgOffset.x(),imgOffset.y());
+
+    painter.setTransform(transform);
+    // 画像のレンダリング
+    painter.drawImage(0,0,scaledImg);
+}
+
+
+void MainWindow::alwaysOnTop() {
+    QSize size = this->size();
+    if(flagTopHint)
+    {
+       // 通常
+       setWindowFlags(windowFlags() & ~Qt::WindowStaysOnTopHint);
+       hide();
+       show();
+       flagTopHint = false;
+       resize(size);
+
+    } else {
+       //　最上面
+       setWindowFlags(Qt::WindowStaysOnTopHint);
+       show();
+       flagTopHint = true;
+       resize(size);
+    }
+
+}
+
+// Event 処理 End ------------------------------
+
+void MainWindow::pasteImage() {
+        qDebug() << "paste";
+        QClipboard* clip = QApplication::clipboard();
+        if (clip->mimeData()->hasImage())
+        {
+
+            QSharedPointer<QImage>
+                imgPtr(QSharedPointer<QImage>::create(clip->image()));
+            // QSharedPointerをQListに追加
+            imgList.append(imgPtr);
+        }
+        curIndex = imgList.length()-1;
+        maxIndex = curIndex;
+        qDebug() << imgList.length();
+        update();
+}
+
+void MainWindow::next() {
+    curIndex ++;
+    if (curIndex > maxIndex) curIndex = 0;
+    update();
+}
+
+void MainWindow::back() {
+    curIndex--;
+    if(curIndex < 0) curIndex = maxIndex;
+    update();
+}
+
+void MainWindow::escape() {
+    close();
+}
+
+void MainWindow::save() {
+    QString timeStamp = QDateTime::currentDateTime().toString("yyyy_MM_dd_HH_mm_ss");
+    QString picturePath = QStandardPaths::writableLocation(QStandardPaths::PicturesLocation)
+                          + "/" + timeStamp;
+    // ディレクトリがなければディレクトリを作る
+    QDir qDir;
+    if (!qDir.exists(picturePath)) {
+        if(!qDir.mkpath(picturePath)) {
+            QMessageBox::critical(this,"エラー","ディレクトリが正しく作られませんでした");
+            return;
+        }
+    }
+
+    int index = 0;
+    for(auto &img:imgList) {
+        img.data()->save(picturePath +"/img_"+ QString::number(index) +".png");
+        index++;
+    }
+
+    // クリップボードに保存パスを設定
+     QClipboard *clip = QApplication::clipboard();
+    clip->setText(picturePath);
+
+    QMessageBox::information
+        (this,"保存完了",picturePath + "に正しく画像を書き込めました\n保存後のパスはクリップボードに取得しました");
+}
+
+
+void MainWindow::hideTabToggle()
+{
+    int wndWidth = width();
+    int wndHeight = height();
+     if (flagFrameHide) {
+         setWindowFlags(windowFlags() & ~Qt::FramelessWindowHint);
+         flagFrameHide = false;
+     } else {
+         setWindowFlags(windowFlags() | Qt::FramelessWindowHint);
+         flagFrameHide = true;
+     }
+     show();
+
+     resize(wndWidth,wndHeight);
+
+}
+
+
+
---
+++ ImgClipper/ImgClipper.h
@@ -0,0 +1,81 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QApplication>
+#include <QClipboard>
+#include <QDateTime>
+#include <QDebug>
+#include <QDir>
+#include <QImage>
+#include <QKeyEvent>
+#include <QList>
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QMimeData>
+#include <QPaintEvent>
+#include <QPainter>
+#include <QRect>
+#include <QSharedPointer>
+#include <QSize>
+#include <QStandardPaths>
+#include <QTransform>
+#include <QVector2D>
+#include <QTextEdit>
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+private:
+    // private enum
+    enum class ViewerMode{Normal,Zoom};
+    ViewerMode viewrMode = ViewerMode::Normal;
+
+    // const value
+    const float ZOOM_INIT = 1.0;
+    const float ZOOM_MAX = 3.0;
+    const float ZOOM_MIN = 0.1;
+    const float ZOOM_DELTA = 0.1;
+    const float OPACITY_INIT = 1.0;
+    const float OPACITY_DELTA = 0.1;
+    const float OFFSET_DELTA = 10;
+
+
+    // private member
+    QTextEdit helpTextEdit;
+    QList<QSharedPointer<QImage>> imgList;
+    int curIndex = -1;
+    int maxIndex = -1;
+    bool flagTopHint = false;
+    bool flagFrameHide = false;
+    float opacity = OPACITY_INIT;
+    float zoomFactor = ZOOM_INIT;
+    QVector2D imgOffset{0,0};
+
+
+    // Event
+    void keyPressEvent(QKeyEvent*) override;
+    void paintEvent(QPaintEvent*) override;
+
+    // mode branch
+    void normalBranch(QKeyEvent*);
+    void zoomBranch(QKeyEvent*);
+
+    // normal mode function
+    void alwaysOnTop();
+    void pasteImage();
+    void next();
+    void back();
+    void escape();
+    void save();
+    void hideTabToggle();
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+
+    ~MainWindow();
+
+private:
+
+
+};
+#endif // MAINWINDOW_H
